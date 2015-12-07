63d5f03d9f066e1b76a3d8c6aa4a48162d988bb6
avadhutp <avadhutp@yelp.com>
2015-12-07 14:23:09 +0000

    Added few rudimentary tests.

--- geddit/feed_model_test.go
+++ geddit/feed_model_test.go
@@ -17,3 +17,35 @@ func TestIsDownloading(t *testing.T) {
 	sut.IsDownloading()
 	assert.Equal(t, IsDownloading, sut.Data.Status)
 }
+
+func TestDownloaded(t *testing.T) {
+	sut := getSut()
+	assert.Zero(t, sut.Data.Status)
+
+	sut.Downloaded()
+	assert.Equal(t, Downloaded, sut.Data.Status)
+}
+
+func TestNotDownloaded(t *testing.T) {
+	sut := getSut()
+	assert.Zero(t, sut.Data.Status)
+
+	sut.CannotDownload()
+	assert.Equal(t, NotDownloaded, sut.Data.Status)
+}
+
+func TestPlaying(t *testing.T) {
+	sut := getSut()
+	assert.Zero(t, sut.Data.Status)
+
+	sut.IsPlaying()
+	assert.Equal(t, Playing, sut.Data.Status)
+}
+
+func TestIsPlayed(t *testing.T) {
+	sut := getSut()
+	assert.Zero(t, sut.Data.Status)
+
+	sut.FinishedPlaying()
+	assert.Equal(t, IsPlayed, sut.Data.Status)
+}
