db926707023ea5834f563e6cf509cead4f9f9e6c
Avadhut P <avadhut.p@gmail.com>
2015-12-08 20:25:49 +0000

    Added tests for player.go

---
+++ ui/player_test.go
@@ -0,0 +1,30 @@
+package ui
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestExpandYoutubeURL(t *testing.T) {
+	tests := []struct {
+		in       string
+		expected string
+		msg      string
+	}{
+		{
+			in:       "https://youtu.be/zKVbJuhcze8",
+			expected: "http://www.youtube.com/watch?v=zKVbJuhcze8",
+			msg:      "Shortened youtu.be url should be expanded correctly",
+		},
+		{
+			in:       "http://[fe80::%31%25en0]:8080/",
+			expected: "http://[fe80::%31%25en0]:8080/",
+			msg:      "Invalid url fails url.Parse and therefore should be returned as is",
+		},
+	}
+
+	for _, test := range tests {
+		actual := expandYoutubeURL(test.in)
+		assert.Equal(t, test.expected, actual, test.msg)
+	}
+}
